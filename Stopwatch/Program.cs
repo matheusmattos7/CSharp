63a32961636115e5ac4f45c354fd1e35af1ea6ea
Matheus mattos <matheusm.mattos7@gmail.com>
2022-12-04 23:56:32 -0300

    Adiciona Stopwatch

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo");
+            Console.WriteLine("M = Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine()!.ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!");
+            Thread.Sleep(1000);
+
+            Start(time);
+        }
+        static void Start(int time)
+        {
+            int CURRENT_TIME = 0;
+
+            while (CURRENT_TIME != time)
+            {
+                Console.Clear();
+                CURRENT_TIME++;
+                Console.WriteLine(CURRENT_TIME);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(1500);
+            Menu();
+        }
+    }
+}
