88c88e0c996f4102803bd6cd4490fed3bd600bfc
Matheus mattos <matheusm.mattos7@gmail.com>
2023-03-13 18:27:01 -0300

    feat: add blog project with Entity Framework

---
+++ EntityFramework/Blog/Data/Mappings/PostMap.cs
@@ -0,0 +1,73 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            // Tabela
+            builder.ToTable("Post");
+
+            // Chave Primária
+            builder.HasKey(x => x.Id);
+
+            // Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(); // PRIMARY KEY IDENTITY(1, 1)       
+
+            // Propriedades
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasMaxLength(60)
+                .HasDefaultValueSql("GETDATE()"); // Default value do SQL
+                                                  // .HasDefaultValue(DateTime.Now.ToUniversalTime()); // Default value do .NET
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            // Indice
+            builder
+                .HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+
+            // Relacionamentos
+            builder
+                .HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Category")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
+        }
+    }
+}
---
+++ EntityFramework/Blog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new BlogDataContext();
+        }
+    }
+}
